754f1a6b4770de4fe914ed740b01818bc6e474f6
Zenzi Ali <zeeathome935@gmail.com>
2020-10-03 15:42:20 -0400

    updated user service

--- src/Adopt.js
+++ src/Adopt.js
@@ -1,42 +1,59 @@
-import React, {Component} from 'react';
-import Context from './Context'
-import Cats from './Cats/Cats'
-import Dogs from './Dogs/Dogs'
-import userService from './services/userService'
+import React, { Component } from "react";
+import Context from "./Context";
+import Cats from "./Cats/Cats";
+import Dogs from "./Dogs/Dogs";
+import userService from "./services/userService";
 
 export default class Home extends Component {
-    static contextType = Context;
+  state = { status: {}, count: 0 };
 
-    componentDidMount() {
-        this.displayQueue()
-    }
+  static contextType = Context;
+  //static interval =
+  //setInterval(() => userService.deleteCurrent, 5000)%%
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.shuffleUser();
+    }, 10000);
+    //this.displayQueue();%%
+  }
+  //check if I'm on my last user? condition
+  shuffleUser = () => {
+    this.context.removeUser(); //updates view
+    const count = this.state.count++;
+    this.setState({ status: {}, count: count });
+    userService.deleteCurrent();
+    this.context.removeUser();
+  };
+  // The currentUser (users[0]) should be deleted every five seconds. The currentUser is located
+  // in state in the app module, and available through Context. Once our logged-in user is at users[0],
+  // the timeout function should stop, allowing the user to choose a pet.
+  // displayQueue = () => {%%
+  //   for (let i = 0; i < this.context.users.length; i++) {%%
+  //     if (this.context.user !== this.context.currentUser) {%%
+  // setInterval(() => userService.deleteCurrent, 5000);%%
+  //     }
+  //   }
+  //   //call a method to force a state update
+  // };
 
-    // The currentUser (users[0]) should be deleted every five seconds. The currentUser is located 
-    // in state in the app module, and available through Context. Once our logged-in user is at users[0],
-    // the timeout function should stop, allowing the user to choose a pet. 
-    displayQueue = () => {
-        for(let i=0; i<this.context.users.length; i++){
-            if(this.context.user !== this.context.currentUser){
-                setTimeout(() => userService.deleteCurrent(), 5000)
-            }
-        }
-    }
+  addToQueue = () => {
+    // There should be a new random user added to the end of the queue every few seconds.
+  };
 
-    addToQueue = () => {
-        // There should be a new random user added to the end of the queue every few seconds.
-    }
-
-
-    render() {
-        return(
-            <div>
-                <h2>{this.context.currentUser} is currently adopting</h2>
-                { /* The Cats and Dogs modules should cooperate with the displayQueue function; 
+  render() {
+    if (this.context.isLoading) {
+      return <p>Loading</p>;
+    } else {
+      return (
+        <div>
+          <h2>{this.context.currentUser} is currently adopting</h2>
+          {/* The Cats and Dogs modules should cooperate with the displayQueue function; 
                 whenever an imaginary user is deleted from the queue, a cat and/or dog should also be deleted.
-                They should also display "Adopted!" or something to that effect whenever they are deleted. */ }
-                <Cats className="pet"/>
-                <Dogs className="pet"/>
-            </div>
-        )
+                They should also display "Adopted!" or something to that effect whenever they are deleted. */}
+          <Cats className="pet" />
+          <Dogs className="pet" />
+        </div>
+      );
     }
+  }
 }
--- src/app/App.js
+++ src/app/App.js
@@ -1,49 +1,68 @@
-import React, { Component } from 'react'
-import {Route, Link} from 'react-router-dom'
-import './App.css'
-import Home from '../Home'
-import Adopt from '../Adopt'
-import Context from '../Context'
-import userService from '../services/userService'
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "../Home";
+import Adopt from "../Adopt";
+import Context from "../Context";
+import userService from "../services/userService";
 
 export default class App extends Component {
   state = {
     user: null,
     isWaiting: false,
-    users: []
-  }
+    users: [],
+    count: 0,
+    isLoading: false,
+  };
 
   setUser = (user) => {
-    this.setState({user: user, isWaiting: true})
-  }
+    this.setState({ isLoading: true });
+    const updatedUsers = this.state.users.push(user);
+    this.setState({
+      user: user,
+      isWaiting: true,
+      users: updatedUsers,
+      isLoading: false,
+    });
+  };
+
+  removeUser = () => {
+    if (this.state.users.length > 0) {
+      this.setState({ count: this.state.count + 1 });
+      const updatedUsers = this.state.users.slice(1, this.state.users.length);
+      this.setState({ users: updatedUsers });
+    }
+  };
 
   componentDidMount() {
-    userService.getUsers()
-    .then(users => {
-        this.setState({users})
-    })
+    userService.getUsers().then((users) => {
+      console.log(users);
+      this.setState({ users });
+    });
   }
-
 
   render() {
     const value = {
       setUser: this.setUser,
+      removeUser: this.removeUser,
       user: this.state.user,
       isWaiting: this.state.isWaiting,
       currentUser: this.state.users[0],
-      users: this.state.users
-    }
+      users: this.state.users,
+      isLoading: this.state.isLoading,
+    };
     return (
       <Context.Provider value={value}>
         <div className="center">
-          <Link to='/'><h1>Petful</h1></Link>
+          <Link to="/">
+            <h1>Petful</h1>
+          </Link>
           <div className="petSection">
-            <Route exact path="/" component={Home}/>
-            <Route path="/adopt" component={Adopt}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/adopt" component={Adopt} />
           </div>
         </div>
       </Context.Provider>
-    )
+    );
   }
 }
-
